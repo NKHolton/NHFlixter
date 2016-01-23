9e4544522c1c1ad1fb0718ca972cbfd7e81646cf
Natalie Holton <natalie.k.holton@gmail.com>
2016-01-23 17:11:09 +0000

    fixed duplicate code for authenticate user and current course user in courses controller and view

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,16 +1,28 @@
 class Instructor::SectionsController < ApplicationController
+	before_action :authenticate_user! #makes sure a user is logged in
+	before_action :require_authorized_for_current_users #makes sure that that user logged in is the one who created the course
+
 	def new
-		@course = Course.find(params[:course_id])
 		@section = Section.new
 	end
 
 	def create
-    	@course = Course.find(params[:course_id])
-    	@section = @course.sections.create(section_params)
-    	redirect_to instructor_course_path(@course)
+    	@section = current_course.sections.create(section_params)
+    	redirect_to instructor_course_path(current_course)
   	end
 
  	private
+
+  	def require_authorized_for_current_course
+  		if current_course.user != current_user
+  			render text: "Unauthorized", status: :unauthorized
+  		end
+  	end
+
+  	helper_method :current_course
+  	def current_course
+  		@current_course ||= Course.find(params[:course_id])
+  	end
 
   	def section_params
     	params.require(:section).permit(:title)
