d5063b762c5b5564b07f3012731085b342c66b38
Natalie Holton <natalie.k.holton@gmail.com>
2016-01-27 15:37:47 +0000

    finished adding enrollments

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,8 +2,8 @@ class EnrollmentsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		current_user.enrollments.create(course: current_course) #current_user = user_id.  This ensures correct data of course_id and user_id gets stored in our database
-		redirect_to course_path(current_course) #found course_path via rake routes for course GET, courses controller, show action
+		current_user.enrollments.create(course: current_course) 
+		redirect_to course_path(current_course) 
 	end
 
 	private
